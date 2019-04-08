e27b39380bc549343237a785873999910ceb3c28
mijohansen <michael.johansen@nav.no>
2019-04-08 18:56:22 +0200

    tmp

--- tests/stream.js
+++ tests/stream.js
@@ -6,6 +6,10 @@ async function testStreaming (conf) {
   conf.params = { UPDATED_TIME: new Date('2018-01-01') }
   conf.params.UPDATED_TIME = conf.params.UPDATED_TIME.toISOString().replace('Z', '-00:00')
   const influx = createInfluxClient(conf)
+  const databaseNames = await influx.getDatabaseNames()
+  if (!databaseNames.includes(conf.influx.database)) {
+    await influx.createDatabase(conf.influx.database)
+  }
   await testStream(conf, (points) => {
     return influx.writePoints(points.splice(0, points.length))
   })
