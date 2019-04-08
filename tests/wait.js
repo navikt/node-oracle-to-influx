8470b8ccd4b21edae4fec4ba881ce402472e0c79
mijohansen <michael.johansen@nav.no>
2019-04-08 18:49:28 +0200

    tmp

--- tests/wait.js
+++ tests/wait.js
@@ -9,17 +9,20 @@ function sleep (millis) {
   return new Promise(resolve => setTimeout(resolve, millis))
 }
 
-async function waitOnOracle (oraOptions, attempt) {
+async function waitOnOracle (oraOptions, attempt, message) {
   try {
     const connection = await oracledb.getConnection(oraOptions)
     const result = await connection.execute(`SELECT sys_context('USERENV','CURRENT_SCHEMA') as db_user, dbtimezone, SESSIONTIMEZONE  from dual`)
-    console.info('connected to oracledatabase ', result.rows[0])
+    console.info('Success, connected to oracledatabase ', result.rows[0])
   } catch (e) {
-    if (attempt < 20) {
-      console.log(e.message)
+    // 2 minutter bør holde for å komme seg på.
+    if (attempt < 120) {
+      if (message !== e.message) {
+        console.log('Trying to connect: ', e.message)
+      }
       attempt++
       await sleep(1000)
-      return waitOnOracle(oraOptions, attempt)
+      return waitOnOracle(oraOptions, attempt, e.message)
     } else {
       console.error('Failed connecting to oracle exiting.')
       process.exit(1)
