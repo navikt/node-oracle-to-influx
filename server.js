bac95dcd09d7130732d026a1b44c8cf737343e80
Michael Johansen <michael.johansen@gmail.com>
2019-04-09 12:34:54 +0200

    npm config (#2)

--- server.js
+++
@@ -1,5 +0,0 @@
-const rawConfig = require('./tests/testConfigs')
-const bootstrap = require('./src').bootstrap
-const port = process.env.PORT || 8081
-
-bootstrap(rawConfig).listen(port)
---
+++ tests/server.js
@@ -0,0 +1,5 @@
+const rawConfig = require('./testConfigs')
+const bootstrap = require('../src').bootstrap
+const port = process.env.PORT || 8081
+
+bootstrap(rawConfig).listen(port)
