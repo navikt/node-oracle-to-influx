23fbf2d32fb875aed722ab3d9d3877142b3c6c1a
mijohansen <michael.johansen@nav.no>
2019-04-10 16:00:03 +0200

    tmp

--- src/linter.js
+++ src/linter.js
@@ -1,6 +1,10 @@
 const lintConfig = function (queryConfig) {
 
 }
+/**
+ * @todo write linter
+ * @param configs
+ */
 module.exports = function (configs) {
   configs.foreach((config) => {
     lintConfig(config)
